75039235ca1b4d00f25bde69c3a64247a57e68a6
araujoalca <andre_araujo@id.uff.br>
2022-02-19 20:18:46 -0300

    First Commit

---
+++ src/br/com/araujoalca/dio/PrimeiroVezIntelliJ.java
@@ -0,0 +1,17 @@
+package br.com.araujoalca.dio;
+
+import br.com.araujoalca.dio.model.Gato;
+
+public class PrimeiroVezIntelliJ {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+        System.out.println("\nOlá, Mundo!!!\nA soma de a + b é " + a + " + " + b + " = " + (a + b));
+
+
+        Gato gato = new Gato();
+        System.out.println("\n" + gato);
+
+    }
+}
