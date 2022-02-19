e138c4c86e3dc32e6c910864950bed928bbbb4ae
araujoalca <andre_araujo@id.uff.br>
2022-02-19 20:28:53 -0300

    Criando class Livro

--- src/br/com/araujoalca/dio/PrimeiroVezIntelliJ.java
+++ src/br/com/araujoalca/dio/PrimeiroVezIntelliJ.java
@@ -11,7 +11,44 @@ public class PrimeiroVezIntelliJ {
 
 
         Gato gato = new Gato();
+        Livro livro = new Livro("A Bíblia Sagrada", 1650);
+        
         System.out.println("\n" + gato);
+        System.out.println("\n" + livro);
 
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
